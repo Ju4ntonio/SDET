1438415a4e08041fb3a57dfaae5114d346c4763d
Jjimenez <juan.jimenez@unosquare.com>
2019-03-18 09:40:33 -0700

    Test1

--- sdet/src/sdet/test.java
+++ sdet/src/sdet/test.java
@@ -4,10 +4,10 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.print("Test1");
 		System.out.print("");
 		System.out.print("");
-		System.out.print("");
-		System.out.print("");
+		System.out.print("Test4");
 	}
 
 }
